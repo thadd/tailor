41cff885de8558a0c7de23cb992186743af5ec78
Thadd Selden <thadd.selden@gmail.com>
2013-01-24 10:17:25 -0800

    Additional styling and reorganization

--- site/js/tailor.js
+++ site/js/tailor.js
@@ -1,17 +1,21 @@
 var LogFile = Backbone.Model.extend({
   completeLogData: '',
   mostRecentLogData: '',
+  filePath: '',
 
   initialize: function() {
     _.bindAll(this);
 
     this.socket = io.connect('http://localhost:8080');
     this.socket.on("log update", this.handleLogUpdate);
+
+    this.on({"change:filePath": this.requestLogFile});
   },
 
-  requestLogFile: function(filePath) {
-    this.filePath = filePath;
-    this.socket.emit('request log', { file_path: filePath });
+  requestLogFile: function() {
+    if ( this.get('filePath').length > 0 ) {
+      this.socket.emit('request log', { file_path: this.get('filePath') });
+    }
   },
 
   handleLogUpdate: function(data) {
@@ -22,42 +26,43 @@ var LogFile = Backbone.Model.extend({
 
 var LogFileView = Backbone.View.extend({
   className: 'log',
+  logId: '',
 
   initialize: function() {
     _.bindAll(this);
 
+    this.listenTo(this.model, "change:filePath", this.renderNewLogFile);
     this.listenTo(this.model, "change:mostRecentLogData", this.render);
+  },
+
+  renderNewLogFile: function() {
+    this.logId = _.uniqueId('log_');
+
+    $('.log-tabs .none').remove();
+    $('.log-tabs ul').append("<li class='active'><a href='#" +
+      this.logId + "'>" +
+      this.model.get('filePath') + "</a></li>");
   },
 
   render: function() {
     var consoleOutput = new ConsoleOutput();
-    consoleOutput.content = this.escapeHtml(this.model.get('mostRecentLogData'));
+    consoleOutput.content = _.escape(this.model.get('mostRecentLogData'));
 
     $('.log-area .log').append(consoleOutput.toHtml()).scrollTop($('.log-area .log')[0].scrollHeight);
-  },
-
-  escapeHtml: function(string) {
-    var entityMap = {
-      "&": "&amp;",
-      "<": "&lt;",
-      ">": "&gt;",
-      '"': '&quot;',
-      "'": '&#39;',
-      "/": '&#x2F;'
-    }
-
-    return String(string).replace(/[&<>"'\/]/g, function(s) {
-      return entityMap[s];
-    });
   }
 });
 
 $(function() {
-  $('.add-file input[type="button"]').click(function() {
+  $('.add-file-path-name').focus();
+
+  $('.add-file-form').submit(function(event) {
     var file = new LogFile();
     var view = new LogFileView({model: file});
 
-    file.requestLogFile( $('.add-file input[type="text"]').val() );
+    file.set({filePath: $('.add-file-path-name').val()})
 
+    $('.add-file-path-name').val('');
+
+    event.preventDefault();
   });
 });
